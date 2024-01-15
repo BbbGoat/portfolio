381aefe4dbd951dd81f79bef35c3e14f5e1ae0fe
BbbGoat <oomi9421@gmail.com>
2024-01-15 22:23:53 +0900

    ss

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.scss";
+import { AiFillGithub } from "react-icons/ai";
+import { SiVelog } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer className={styles.footer}>
+        <div className={styles.wrap}>
+          <div className={styles.inner}>
+            <div className={styles.left}>
+              <div className={styles.text_box}>
+                <p>엥</p>
+                <span>우에엥</span>
+              </div>
+              <div className={styles.text_box}>
+                <p>Mail.</p>
+                <div>
+                  <span>oomi9421@gmail.com</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.right}>
+              <div className={styles.text_box}>
+                <p>Github.</p>
+                <div>
+                  <AiFillGithub size={20} />
+                  <span>BbbGoat</span>
+                </div>
+              </div>
+              <div className={styles.text_box}>
+                <p>Velog.</p>
+                <div>
+                  <SiVelog size={20} />
+                  <span>BbbGoat</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
 import styles from './Header.module.scss'
 
 const Header = () => {
+
+  const { pathname } = useLocation();
+  const path = pathname.slice(1);
 
   const category = ['About', 'Portfolio', 'Skills']
   
@@ -10,23 +13,25 @@ const Header = () => {
     <>
       <header className={styles.header}>
         <div className={styles.wrap}>
-          <div className={styles.logo}>
-            <h1>BBBGOAT</h1>
-          </div>
-          <div className={styles.category}>
-            <ul>
-              {
-                category.map((item, idx)=>{
-                  return (
-                    <li key={idx}>
-                      <Link to={`/${item.toLowerCase()}`}>
-                        <span>{item}</span>
-                      </Link>
-                    </li>
-                  )
-                })
-              }
-            </ul>
+          <div className={styles.inner}>
+            <div className={styles.logo}>
+              <h1>BBBGOAT</h1>
+            </div>
+            <div className={styles.category}>
+              <ul>
+                {
+                  category.map((item, idx)=>{
+                    return (
+                      <li key={idx} className={path === item.toLowerCase() ? styles.active : ''}>
+                        <Link to={`/${item.toLowerCase()}`}>
+                          <span>{item}</span>
+                        </Link>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            </div>
           </div>
         </div>
       </header>
