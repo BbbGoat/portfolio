cfecba72ad6e2b40e359ecd4c9b11c9e8730e4cc
BbbGoat <oomi9421@gmail.com>
2024-01-15 17:34:21 +0900

    Update Header.tsx

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -2,13 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+
+  const category = ['About', 'Portfolio', 'Skills', 'Contact']
+  
   return (
     <>
+      <header>
         <div>
-            
+          {
+            category.map((item, idx)=>{
+              return (
+                <Link to={`/${item.toLowerCase()}`} key={idx}>
+                  <span>{item}</span>
+                </Link>
+              )
+            })
+          }
         </div>
-      <Link to={"/about"}>이동</Link>
-      <div>Header</div>
+      </header>
     </>
   );
 };
