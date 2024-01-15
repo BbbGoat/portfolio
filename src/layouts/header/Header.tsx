9e14625699e7c1a9f028ef5002b09849d0a4b503
BbbGoat <oomi9421@gmail.com>
2024-01-15 17:27:25 +0900

    ss

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-        <Link to={'/about'}>이동</Link>
-        <div>Header</div>
+        <div>
+            
+        </div>
+      <Link to={"/about"}>이동</Link>
+      <div>Header</div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
