b6e9842884c59a3603fa4d0c3590608bc771746c
BbbGoat <oomi9421@gmail.com>
2024-01-16 13:27:39 +0900

    ss

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -19,14 +19,14 @@ const Header = () => {
   
   return (
     <>
-      <header className={styles.header}>
+      <header className={active ? `${styles.header} ${styles.active}` : styles.header}>
         <div className={styles.wrap}>
           <div className={styles.inner}>
             <div className={styles.logo}>
               <h1>BBBGOAT</h1>
             </div>
             <div className={styles.category}>
-              <ul>
+              <ul className={styles.pc_list}>
                 {
                   category.map((item, idx)=>{
                     return (
@@ -39,7 +39,10 @@ const Header = () => {
                   })
                 }
               </ul>
-              <a className={active ? `${styles.menu_trigger} ${styles.active}` : styles.menu_trigger} href="#" 
+              {/* 햄버거 버튼 */}
+              <a 
+                className={active ? `${styles.menu_trigger} ${styles.active}` : styles.menu_trigger} 
+                href="#" 
                 onClick={(e)=>{handleClick(e)}}>
                 <span></span>
                 <span></span>
@@ -48,6 +51,20 @@ const Header = () => {
             </div>
           </div>
         </div>
+        {/* 모바일용 ul */}
+        <ul className={active ? `${styles.m_list} ${styles.active}` : styles.m_list}>
+          {
+            category.map((item, idx)=>{
+              return (
+                <li key={idx}>
+                  <Link to={`/${item.toLowerCase()}`}>
+                    <span>{item}</span>
+                  </Link>
+                </li>
+              )
+            })
+          }
+        </ul>
       </header>
     </>
   );
