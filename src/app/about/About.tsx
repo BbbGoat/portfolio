196f2e02b573467e46d2e5d32fde36d4d31d5a3e
BbbGoat <oomi9421@gmail.com>
2024-01-22 23:02:33 +0900

    ss

--- src/app/about/About.tsx
+++ src/app/about/About.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import styles from './About.module.scss'
+import React from "react";
+import styles from "./About.module.scss";
 
 const About = () => {
   return (
     <section className={styles.about}>
-        <div>About</div>
+      <div>About</div>
+      <div>
+        
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
