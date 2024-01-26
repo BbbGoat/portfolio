6f5b99669f1ec80d74988746ef46a781874d33a3
BbbGoat <oomi9421@gmail.com>
2024-01-26 16:53:17 +0900

    ss

--- src/app/about/About.tsx
+++ src/app/about/About.tsx
@@ -4,18 +4,85 @@ import Heading from "../../components/Heading";
 
 const About = () => {
   return (
-    <section className={styles.about}>
+    <article className={styles.about}>
       <Heading 
         title="About"
         subTitle="If you should put in the text this will be subTitle Apple Banana" 
         emoji="/emojis/heart.png"
       />
-      <div className={styles.content}>
-        <div className={styles.container}>
-          여기부터는 섹션별 메인 콘텐츠가 들어갑니다! 
+      <section className={styles.sec1}>
+        <div className={styles.inner}>
+          <div className={styles.press}>
+            <h2 className={styles.sec_title}>
+              <span>Press</span>
+            </h2>
+            <ul>
+              <li>
+                <h4 className={styles.year}>
+                  <span className={styles.txt}>
+                    <span>2023</span>
+                  </span>
+                </h4>
+                <ul className={styles.detail}>
+                  <li>
+                    <ul className={styles.lists}>
+                      <li className={styles.item}>
+                        <button>
+                          <span>Sodssd jSod mEj jcmek <br/> Wef mlkd fWmnvl</span>
+                        </button>
+                        <p>
+                          추가 내용 추가 내용
+                        </p>
+                      </li>
+                      <li className={styles.item}>
+                        <button>
+                          <span>Sodssd jSod mEj jcmek <br/> Wef mlkd fWmnvl</span>
+                        </button>
+                        <p>
+                          추가 내용 추가 내용
+                        </p>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+
+              {/* 임시로 반복! */}
+              <li>
+                <h4 className={styles.year}>
+                  <span className={styles.txt}>
+                    <span>2023</span>
+                  </span>
+                </h4>
+                <ul className={styles.detail}>
+                  <li>
+                    <ul className={styles.lists}>
+                      <li className={styles.item}>
+                        <button>
+                          <span>Sodssd jSod mEj jcmek <br/> Wef mlkd fWmnvl</span>
+                        </button>
+                        <p>
+                          추가 내용 추가 내용
+                        </p>
+                      </li>
+                      <li className={styles.item}>
+                        <button>
+                          <span>Sodssd jSod mEj jcmek <br/> Wef mlkd fWmnvl</span>
+                        </button>
+                        <p>
+                          추가 내용 추가 내용
+                        </p>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </article>
   );
 };
 
