b8328cc00e4b99eae56beecb879cb7cec811d099
BbbGoat <oomi9421@gmail.com>
2024-01-29 17:39:16 +0900

    ss

--- src/app/about/About.tsx
+++ src/app/about/About.tsx
@@ -5,8 +5,11 @@ import AboutData from "./AboutList";
 
 const About = () => {
   const [addClass, setAddClass] = useState<number[]>([]);
+  const [career, setCareer] = useState(false);
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
+    e.preventDefault();
+
     if (addClass.includes(id)) {
       return setAddClass(addClass.filter((item)=>item !== id));
     }
@@ -20,11 +23,11 @@ const About = () => {
         subTitle="If you should put in the text this will be subTitle Apple Banana"
         emoji="/emojis/heart.png"
       />
-      <section className={styles.sec1}>
+      <section className={styles.section}>
         <div className={styles.inner}>
-          <div className={styles.press}>
+          <div className={styles.container}>
             <h2 className={styles.sec_title}>
-              <span>Press</span>
+              <span>Education</span>
             </h2>
             <ul>
               {/* 반복 */}
@@ -78,6 +81,118 @@ const About = () => {
                   </li>
                 );
               })}
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.section}>
+        <div className={styles.inner}>
+          <div className={styles.container}>
+            <h2 className={styles.sec_title}>
+              <span>Career</span>
+            </h2>
+            <ul>
+              <li>
+                <h4 className={styles.year}>
+                  <span className={styles.txt}>
+                    <span>2020</span>
+                  </span>
+                </h4>
+                <ul className={styles.detail}>
+                  <li>
+                    <ul className={styles.lists}>
+
+                      <li className={styles.item}>
+                        <button onClick={()=>setCareer(!career)}>
+                          <span>
+                            ㈜노매드코리아 - 웹디자이너 ↙ <br /> (2020.02 - 2022.12　 
+                            <small>
+                              2년 11개월
+                            </small>
+                            )
+                          </span>
+                        </button>
+                        <ol>
+                          <li className={career ? styles.on : ''}>
+                            <span>
+                            - 기획전 배너, 이벤트 페이지, 상세페이지 제작 <br />
+                            - 홈페이지 유지 보수 (HTML, CSS 퍼블리싱) <br/>
+                            - SNS 마케팅 영상 제작 <br />
+                            </span>
+                          </li>
+                        </ol>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.section}>
+        <div className={styles.inner}>
+          <div className={styles.container}>
+            <h2 className={styles.sec_title}>
+              <span>Certificate</span>
+            </h2>
+            <ul>
+              <li>
+                <h4 className={styles.year}>
+                  <span className={styles.txt}>
+                    <span>2019</span>
+                  </span>
+                </h4>
+                <ul className={styles.detail}>
+                  <li>
+                    <ul className={styles.lists}>
+                      <li className={styles.item}>
+                        <button>
+                          <span>
+                            GTQ 일러스트 1급<br /> (2019.07 <small>최종합격</small>)
+                          </span>
+                        </button>
+                      </li>
+                      <li className={styles.item}>
+                        <button>
+                          <span>
+                            GTQ 포토샵 1급<br /> (2019.06 <small>최종합격</small>)
+                          </span>
+                        </button>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <h4 className={styles.year}>
+                  <span className={styles.txt}>
+                    <span>2017</span>
+                  </span>
+                </h4>
+                <ul className={styles.detail}>
+                  <li>
+                    <ul className={styles.lists}>
+                      <li className={styles.item}>
+                        <button>
+                          <span>
+                          Apple Certified Pro Final Cut Pro X (Level Two)<br /> (2017.09 <small>최종합격</small>)
+                          </span>
+                        </button>
+                      </li>
+                      <li className={styles.item}>
+                        <button>
+                          <span>
+                          SMAT 서비스경영자격<br /> (2017.08 <small>최종합격</small>)
+                          </span>
+                        </button>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
             </ul>
           </div>
         </div>
--- src/app/about/AboutList.ts
+++ src/app/about/AboutList.ts
@@ -5,7 +5,7 @@ const AboutList = [
       {
         id: 1,
         title: "프로젝트기반 프론트엔드(React, Vue) 웹&앱 SW개발자 양성과정 ↙",
-        content: "개인 프로젝트 기반의 6개월 프론트엔드 개발자 양성 과정으로 하이브리드앱/ PWA(Progressive Web Apps) 제작, Vue.js/React.js 프레임워크를 사용한 SPA 사이트 개발과 Git을 사용한 형상관리 & 배포 과정 학습",
+        content: "개인 프로젝트 기반의 6개월 프론트엔드 개발자 양성 과정 수료 ^ 모바일 웹&앱 제작 하이브리드앱/ PWA(Progressive Web Apps) 제작 ^ Vue.js/React.js 프레임워크를 사용한 SPA 사이트 개발과 Git을 사용한 형상관리 & 배포 과정 학습",
         date: "2022.12 - 2023.07",
       },
     ],
@@ -16,7 +16,7 @@ const AboutList = [
       {
         id: 2,
         title: "디지털디자인 반응형 웹디자인 과정 ↙",
-        content: "디지털 디자인 (반응형 웹디자인) 3개월 과정 수료 ^ 반응형 웹페이지를 기획/제작하고 Photoshop, Illustrator, HTML5, CSS3, Javascript 기초 학습",
+        content: "디지털 디자인 (반응형 웹디자인) 3개월 과정 수료 ^ 웹 퍼블리싱 & 디자인 중점의 과정으로 반응형 웹페이지를 직접 기획/제작함 ^ Photoshop, Illustrator, HTML5, CSS3, Javascript 기초 학습",
         date: "2019.05 - 2019.07",
       },
     ],
